a3393e56e07c30bb7fdb8ea6fcaaa97f0a0751b3
untalrobert <untalrobertt@gmail.com>
2018-08-22 16:20:48 -0500

    navegacion

--- send_mail.php
+++ send_mail.php
@@ -2,24 +2,20 @@
 	
 	$name = '';
 	$email = '';
-	$subject = '';
 	$message = '';
 	$phone = '';
-	$ciudad = '';
 
 
 	if(isset($_POST['name'])) 		$name = $_POST['name'];
 	if(isset($_POST['email'])) 		$email = $_POST['email'];
-	if(isset($_POST['subject'])) 	$subject = $_POST['subject'];
 	if(isset($_POST['message'])) 	$message = $_POST['message'];
 	if(isset($_POST['phone'])) 	    $message = $_POST['phone'];
-	if(isset($_POST['ciudad'])) 	$message = $_POST['ciudad'];
 
 	$headers = "From: " . $email;
 
 	$to = 'cuentas@enalgunlugarestudio.com';
 
-	mail($to, $subject, 'Nombre: '.$name.' Mensaje: '.$message. ' Ciudad: '.$ciudad. ' Telefono: '.$phone. ' Email: '.$email);
+	mail($to,  'Nombre: '.$name. ' Mensaje: '.$message. ' Teléfono: '.$phone. ' Email: '.$email);
 
 
 ?>
