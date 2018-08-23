efea9d44b50759083096ed199bb8de3ea1175547
untalrobert <untalrobertt@gmail.com>
2018-08-23 10:42:03 -0500

    video

--- js/main.js
+++ js/main.js
@@ -89,37 +89,74 @@ $(document).on('ready', function () {
 // section video
 
 
-var videoPlayButton,
-	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-        renderVideoPlayButton: function() {
-            if (videoWrapper.contains(video)) {
-				this.formatVideoPlayButton()
-                video.classList.add('has-media-controls-hidden')
-                videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
-                videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
-            }
-        },
+// poster frame click event
+$(document).on('click','.js-videoPoster',function(ev) {
+    ev.preventDefault();
+    var $poster = $(this);
+    var $wrapper = $poster.closest('.js-videoWrapper');
+    videoPlay($wrapper);
+  });
+  
+  // play the targeted video (and hide the poster frame)
+  function videoPlay($wrapper) {
+    var $iframe = $wrapper.find('.js-videoIframe');
+    var src = $iframe.data('src');
+    // hide poster
+    $wrapper.addClass('videoWrapperActive');
+    // add iframe src in, starting the video
+    $iframe.attr('src',src);
+  }
+  
+  // stop the targeted/all videos (and re-instate the poster frames)
+  function videoStop($wrapper) {
+    // if we're stopping all videos on page
+    if (!$wrapper) {
+      var $wrapper = $('.js-videoWrapper');
+      var $iframe = $('.js-videoIframe');
+    // if we're stopping a particular video
+    } else {
+      var $iframe = $wrapper.find('.js-videoIframe');
+    }
+    // reveal poster
+    $wrapper.removeClass('videoWrapperActive');
+    // remove youtube link, stopping the video from playing in the background
+    $iframe.attr('src','');
+  }
 
-        formatVideoPlayButton: function() {
-            videoWrapper.insertAdjacentHTML('beforeend', '\
-                <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
-                    <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
-                    <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
-                </svg>\
-            ')
-        },
 
-        hideVideoPlayButton: function() {
-            video.play()
-            videoPlayButton.classList.add('is-hidden')
-            video.classList.remove('has-media-controls-hidden')
-            video.setAttribute('controls', 'controls')
-        }
-	}
 
-videoMethods.renderVideoPlayButton()
+
+// var videoPlayButton,
+// 	videoWrapper = document.getElementsByClassName('video-wrapper')[0],
+//     video = document.getElementsByTagName('video')[0],
+//     videoMethods = {
+//         renderVideoPlayButton: function() {
+//             if (videoWrapper.contains(video)) {
+// 				this.formatVideoPlayButton()
+//                 video.classList.add('has-media-controls-hidden')
+//                 videoPlayButton = document.getElementsByClassName('video-overlay-play-button')[0]
+//                 videoPlayButton.addEventListener('click', this.hideVideoPlayButton)
+//             }
+//         },
+
+//         formatVideoPlayButton: function() {
+//             videoWrapper.insertAdjacentHTML('beforeend', '\
+//                 <svg class="video-overlay-play-button" viewBox="0 0 200 200" alt="Play video">\
+//                     <circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff"/>\
+//                     <polygon points="70, 55 70, 145 145, 100" fill="#fff"/>\
+//                 </svg>\
+//             ')
+//         },
+
+//         hideVideoPlayButton: function() {
+//             video.play()
+//             videoPlayButton.classList.add('is-hidden')
+//             video.classList.remove('has-media-controls-hidden')
+//             video.setAttribute('controls', 'controls')
+//         }
+// 	}
+
+// videoMethods.renderVideoPlayButton()
     
 });
 
